e56b914e928d12009ba17ef86d0bcc32c701cf63
tsloveme <472960087@qq.com>
2015-06-11 00:55:07 +0800

    项目列表的页面样式响应布局设计

---
+++ list.php
@@ -0,0 +1,68 @@
+﻿<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>项目列表</title>
+<meta http-equiv="X-UA-Compatible" content="IE=Edge" />
+<meta name="renderer" content="webkit">
+<meta name="viewport" content="width=device-width initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
+<style type="text/css">
+*{margin:0; padding:0;}
+html{font-size:16px;}
+table{border-collapse:collapse;}
+td,th{padding:.5rem; line-height:1.5; border:1px #ccc solid; word-break:break-all;}
+td{padding:.25em .5em;}
+body{font-size:1rem;font-family:'微软雅黑';}
+@media screen  and (min-width:359px) and (max-width:414px) {
+	body{font-size:1rem;}
+}
+@media screen  and (max-width:359px){
+	body{font-size:.75rem;}
+}
+@media screen  and (max-width: 799px){
+	body{max-width:480px; margin:0 auto;}
+	.main{width:auto; max-width:480px; margin:0 .5rem;}
+}
+@media screen  and (min-width: 800px){
+	.main{width:720px; margin:0 auto;}
+}
+h1{font-size:1.5rem; line-height:2.4; text-align:center; margin.5rem 0;}
+
+</style>
+</head>
+<body>
+	<div class="main">
+		<h1>项目列表</h1>
+		<table border="0" cellspacing="0" cellspadding="0" width="100%">
+			<tr>
+				<th>PC项目</th>
+				<th>移动端项目</th>
+			</tr>
+			<tr>
+				<td><a href="javascript:;">项目1项目1项目1项目1</a></td>
+				<td><a href="javascript:;">项目3333333333333333</a></td>
+			</tr>
+			<tr>
+				<td><a href="javascript:;">项目1项目1项目1项目1</a></td>
+				<td><a href="javascript:;">项目3333333333333333</a></td>
+			</tr>
+			<tr>
+				<td><a href="javascript:;">项目1项目1项目1项目1</a></td>
+				<td><a href="javascript:;">项目3333333333333333</a></td>
+			</tr>
+			<tr>
+				<td><a href="javascript:;">项目1项目1项目1项目1</a></td>
+				<td><a href="javascript:;">项目3333333333333333</a></td>
+			</tr>
+			<tr>
+				<td><a href="javascript:;">项目1项目1项目1项目1</a></td>
+				<td><a href="javascript:;">项目3333333333333333</a></td>
+			</tr>
+			<tr>
+				<td><a href="javascript:;">项目1项目1项目1项目1</a></td>
+				<td><a href="javascript:;">项目3333333333333333</a></td>
+			</tr>
+		</table>
+	</div>
+</body>
+</html>
